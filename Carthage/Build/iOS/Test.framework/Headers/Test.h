94786766a2b8f2196f37ed66f27b4bc1ea6da2cb
JingZhao <jingzhao@thoughtworks.com>
2017-05-16 11:26:19 +0800

    init

---
+++ Carthage/Build/iOS/Test.framework/Headers/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by JingZhao on 5/16/17.
+//  Copyright © 2017 JingZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Test.
+FOUNDATION_EXPORT double TestVersionNumber;
+
+//! Project version string for Test.
+FOUNDATION_EXPORT const unsigned char TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test/PublicHeader.h>
+
+
